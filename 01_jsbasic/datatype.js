74a9c8b2681c8be629bf507843871a69349f8a29
kg-soumen <soumenmallick884@gmail.com>
2025-05-08 17:40:36 +0530

    Create datatype.js

---
+++ 01_jsbasic/datatype.js
@@ -0,0 +1,24 @@
+let score = "95asd"
+
+
+// console.log(typeof score)
+
+let inNumber = Number(score)
+// console.log(typeof inNumber)
+// console.log(inNumber)
+
+let Isprice = "papu"
+let somePrice = Boolean(Isprice);
+console.log( somePrice)
+
+/*
+
+0-false , 1- true
+""-false , "papu" - true
+
+*/
+
+let IsNumber = 33
+let _NumberString = String(IsNumber)
+console.log(_NumberString)
+console.log(typeof _NumberString)
