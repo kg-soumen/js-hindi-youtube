0b0c0cb0dc24625551cf3110278707ffcaf7024c
kg-soumen <soumenmallick884@gmail.com>
2025-05-08 16:44:19 +0530

    Create varioble.js

---
+++ 01_jsbasic/varioble.js
@@ -0,0 +1,14 @@
+const accountId = 1234567
+let accountEmail  = "papu@12.com"
+var accountpassword = "127478"
+accountCity= "joipur"
+
+
+accountEmail="happy@435.com"
+accountpassword="5472"
+accountCity="bangaluru"
+let accountState ;
+
+// accountId = 244687
+console.log(accountId)
+console.table([accountId,accountEmail,accountpassword,accountCity,accountState])
