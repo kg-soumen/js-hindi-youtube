a505cdc702e37082a3730d2143fdf33fb9674fd9
kg-soumen <soumenmallick884@gmail.com>
2025-05-11 09:33:45 +0530

    Create operator.js

---
+++ 01_jsbasic/operator.js
@@ -0,0 +1,14 @@
+// console.log(null >0)//f
+// console.log(null == 0)//f
+// console.log(null <=0)//t
+// console.log(null <0) //f
+// console.log(null >=0) // t
+
+
+// console.log(undefined == 0)
+// console.log(undefined== null)
+// console.log(undefined>=0)
+// console.log(undefined <= null)
+
+
+// null and undefined should both not use//
