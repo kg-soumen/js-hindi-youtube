1d3cadf5b48db66baabcc29741e1ac9430df9e1d
kg-soumen <soumenmallick884@gmail.com>
2025-05-12 16:16:03 +0530

    Update datatype.js

--- 01_jsbasic/datatype.js
+++ 01_jsbasic/datatype.js
@@ -7,9 +7,9 @@ let inNumber = Number(score)
 // console.log(typeof inNumber)
 // console.log(inNumber)
 
-let Isprice = "papu"
-let somePrice = Boolean(Isprice);
-console.log( somePrice)
+// let Isprice = "papu"
+// let somePrice = Boolean(Isprice);
+// console.log( somePrice)
 
 /*
 
@@ -18,7 +18,26 @@ console.log( somePrice)
 
 */
 
-let IsNumber = 33
-let _NumberString = String(IsNumber)
-console.log(_NumberString)
-console.log(typeof _NumberString)
+// let IsNumber = 33
+// let _NumberString = String(IsNumber)
+
+// console.log(typeof _NumberString);
+
+// let myname = "soumenmallick@12.comm"
+// let othername = myname
+
+othername= "papai@dot.com"
+console.log(othername)
+console.log(othername)
+
+//object change
+
+let myobj= {
+    email : "sou@12.com",
+    city: "burdwan"
+}
+
+let otherobj= myobj
+otherobj.email="papuhero@12.com"
+console.log(myobj.email)
+console.log(myobj.email)
