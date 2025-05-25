ecd1a448f912ed9d9f9c9ec321f85e98cfde6944
kg-soumen <soumenmallick884@gmail.com>
2025-05-26 00:14:24 +0530

    Update string.js

--- 01_jsbasic/string.js
+++ 01_jsbasic/string.js
@@ -1,3 +1,15 @@
 let  name = "papu"
 let repocounnt  = 220
 console.log(`your name is ${name} and your repocount is ${repocounnt}`) 
+
+const game = new String('abhimunno')
+// console.log(game[0])
+// console.log(game.__proto__)
+
+const Name = new String('codewithpapu')
+console.log(Name.charAt(3))
+
+console.log(Name.indexOf('p'))
+
+console.log(Name.length)
+console.log(Name.slice(-12, 6))
