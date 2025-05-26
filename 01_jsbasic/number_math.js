801dfa59dc9a4e61519e77630add13902f587f5d
kg-soumen <soumenmallick884@gmail.com>
2025-05-26 11:36:38 +0530

    Create number_math.js

---
+++ 01_jsbasic/number_math.js
@@ -0,0 +1,33 @@
+
+// const balance = new Number(100);
+// console.log(balance)
+
+// console.log(balance.toString().length)
+
+// console.log(balance.toFixed(1));
+
+// const othernumber = 123.8945;
+// console.log(othernumber.toPrecision(3))
+// console.log(othernumber.toPrecision(4))
+// console.log(othernumber.toPrecision(2))
+
+
+// const hundreds = 100000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ++++++++Math  +++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.floor(0.5));
+// console.log(Math.max(6, 7 , 8, 4 ));
+
+// console.log(Math.random());
+// console.log((Math.random()*10 ) + 1);
+// console.log(Math.floor(Math.random() *10) +1);
+
+
+const Max= 10;
+const Min = 20;
+console.log(Math.floor (Math.random()*(Min - Max +1 )) + Max)
