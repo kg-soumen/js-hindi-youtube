d69fc821a37365bed8d44aa10f997a5c1298348a
kg-soumen <soumenmallick884@gmail.com>
2025-05-26 13:25:37 +0530

    Create date_time.js

---
+++ 01_jsbasic/date_time.js
@@ -0,0 +1,48 @@
+// Date
+let MyDate = new Date();
+// console.log(MyDate.toString());
+// console.log(MyDate.setFullYear());
+// console.log(MyDate.setFullYear(2004));
+// console.log(MyDate.getMonth());
+// console.log(MyDate.setDate());
+// console.log(MyDate.toISOString());
+// console.log(MyDate.toJSON());
+// console.log(MyDate.toDateString());
+// console.log(MyDate.toLocaleDateString());
+
+// console.log(typeof MyDate)
+
+// let mycreatedate = new Date(2004 , 0 , 23 );
+// console.log(mycreatedate.toDateString());
+
+
+// let mycreatedate = new Date(2004 , 0 , 23, 6, 5);
+// console.log(mycreatedate.toLocaleString());
+
+
+let mycreatedate = new Date("2003-05-12");
+// console.log(mycreatedate.toLocaleString());
+
+let mydate =  Date.now()
+// console.log(mydate)
+
+
+// console.log(mycreatedate.getTime())
+
+// second a convert korlam
+// console.log(Math.floor(Date.now()/1000))
+
+let newdate = new Date()
+console.log(newdate.getDate())
+console.log(newdate.getMonth())
+console.log(newdate.getDay())
+
+
+
+
+
+ 
+
+
+
+
