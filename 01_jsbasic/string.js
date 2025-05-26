cc587ff89386019f89c43806a6e46829a851fc81
kg-soumen <soumenmallick884@gmail.com>
2025-05-26 09:43:37 +0530

    Update string.js

--- 01_jsbasic/string.js
+++ 01_jsbasic/string.js
@@ -7,9 +7,34 @@ const game = new String('abhimunno')
 // console.log(game.__proto__)
 
 const Name = new String('codewithpapu')
-console.log(Name.charAt(3))
+console.log(Name.charAt(3))//3 no indexx ar character ta pabo
 
-console.log(Name.indexOf('p'))
+console.log(Name.indexOf('p'))//p character ta kotay present seii index ta dekhabe
 
-console.log(Name.length)
-console.log(Name.slice(-12, 6))
+console.log(Name.length)// 
+
+console.log(Name.slice(-10, 6))//  "-" mne back theke count krbe r  plus mnne  samne theke count korbe 
+
+
+// includes meethod 
+// const browsertype = "mozilla"
+// if(browsertype.includes("zilla")){// value por por same hole true hobe att na hole false part
+//     console.log("yes this is found")
+    
+// }
+// else{
+//     console.log("no this is not found")
+    
+// }
+
+// 
+
+const browsertyp = "mozilla"
+if(browsertyp.startsWith("o")){ 
+    console.log("yes this is found")
+    
+}
+else{
+    console.log("no this is not found")
+    
+}
