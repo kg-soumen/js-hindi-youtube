5ad6fee47e2c69241d818745817510c3f157cf29
kg-soumen <soumenmallick884@gmail.com>
2025-06-05 12:37:33 +0530

    Create Array.js

---
+++ 02_basic/Array.js
@@ -0,0 +1,64 @@
+//array
+const Arr1 = [1 , 5, 6 , 7]
+const Array1 = ["papai","Mou", "papu"]
+const arr1 = new Array(1, 4, 6, 8)
+
+
+// console.log(arr1.length);
+Arr1.push(9)
+Arr1.push(8)
+Arr1.push(3)
+// console.log(Arr1.pop())
+
+// console.log(Arr1)
+
+/*
+Arr1.unshift(45)
+Arr1.unshift(55)
+console.log(Arr1.shift())
+console.log(Arr1.shift())
+console.log(Arr1.shift())
+console.log(Arr1.shift())
+console.log(Arr1)
+*/
+/*
+console.log(Arr1.includes(9))
+console.log(Arr1.indexOf(3))
+*/
+
+/*
+const newarr =Arr1.join()
+console.log(newarr)
+console.log(typeof newarr)
+*/
+
+/*
+// slice and splice method 
+console.log("S" , Arr1)
+const  arn = Arr1.slice(1,3)
+console.log(arn)
+console.log("TK",Arr1)
+
+const  arn1 = Arr1.splice(1,3)
+const arn2= ( Arr1)
+console.log(arn1)
+console.log("c",arn2)
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
